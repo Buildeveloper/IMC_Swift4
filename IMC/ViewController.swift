bf25b9fe525515515883242a6920ca8bdae05c4c
Paulo Henrique <Henrique@paulos-mbp.lan>
2019-07-30 23:23:51 -0300

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Paulo Henrique on 30/07/19.
+//  Copyright © 2019 Paulo Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
