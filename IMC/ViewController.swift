33585517b421fb2418e9b681d970691f3693c714
Paulo Henrique <Henrique@Paulos-MBP.lan>
2019-07-31 00:54:21 -0300

    view e constraints finalizada

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        
+    }
+    
 }
 
